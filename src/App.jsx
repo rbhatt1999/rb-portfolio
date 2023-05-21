5b4db81d7accd11cb940a284fcea611d6cf92bf7
Rohit bhatt <rbhatt199924@gmail.com>
2023-05-21 19:30:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/project" element={<Project />} />
         <Route path="/tech" element={<Tech />} />
+        <Route path="*" element={<Home />}/>
       </Routes>
     </div>
   )
