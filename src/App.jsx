74074ed85f5871d212a3f52b1fbbced6b14d84f3
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-21 19:05:27 +0530

    place browerRouter in main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import NavbarHeader from './components/NavbarHeader'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Project from './pages/Project'
@@ -9,7 +9,6 @@ import Tech from './pages/Tech'
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
       <NavbarHeader />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -17,7 +16,6 @@ function App() {
         <Route path="/project" element={<Project />} />
         <Route path="/tech" element={<Tech />} />
       </Routes>
-      </BrowserRouter>
     </div>
   )
 }
